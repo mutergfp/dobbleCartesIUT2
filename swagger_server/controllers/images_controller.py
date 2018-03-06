be7653b1b2f762a63467d5504822058ff82374d1
Maël Gaonach <mael@gaonach.com>
2018-03-06 08:48:40 +0100

    Implémentation ajout d'images

---
+++ swagger_server/controllers/images_controller.py
@@ -0,0 +1,71 @@
+import connexion
+from swagger_server.models.card import Card
+from datetime import date, datetime
+from typing import List, Dict
+from six import iteritems
+from ..util import deserialize_date, deserialize_datetime
+
+from fonctionsGlobales import getImages
+import sqlite3
+import glob
+from flask import make_response
+import imghdr
+
+def add_img(img):
+	"""
+	Ajoute une image aux possibilités pour la génération des cartes
+	
+	:param img: image à ajouter
+	:type img: werkzeug.datastructures.FileStorage
+
+	:rtype: None
+	"""
+	if imghdr.what(img) is 'png':
+		conn = sqlite3.connect('DobbleImages.sqlite')
+		ablob = img.read()
+		sql = '''INSERT INTO IMAGES
+		(IMAGE)
+		VALUES(?);'''
+		conn.execute(sql,[sqlite3.Binary(ablob)])
+		conn.commit()
+		return 'successful operation'
+	else:
+		return make_response("Invalid image supplied", 400)
+
+
+def get_images():
+	"""
+	Retourne la liste de toutes les images
+	
+
+	:rtype: List[Card]
+	"""	
+	return getImages()
+
+
+def get_img(id):
+	"""
+	Retourne l&#39;image correspondante à un ID
+	
+	:param id: Id de l&#39;image à récupèrer
+	:type id: int
+
+	:rtype: file
+	"""
+	conn = sqlite3.connect('DobbleImages.sqlite')
+	
+	sql = "SELECT COUNT(*) FROM IMAGES"
+	cursor = conn.cursor()
+	cursor.execute(sql)	
+	res = cursor.fetchone()
+	if res[0] < id:
+		return make_response("Invalid id supplied", 400)
+	else:
+		sql = "SELECT IMAGE FROM IMAGES WHERE ID = " + str(id)
+		cursor = conn.cursor()
+		cursor.execute(sql)	
+		res = cursor.fetchone()
+		if res[0]:
+			return res[0]
+		else: 
+			return make_response("Invalid id supplied", 400)
