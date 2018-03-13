142e4acc4b7914f67789fbe4f1ccb769e1ca779e
adubre01 <aubin.dubreuil@etudiant.univ-lr.fr>
2018-03-13 15:45:20 +0100

    Modification erreur sur le getImage()

--- fonctionsGlobales.py
+++ fonctionsGlobales.py
@@ -3,6 +3,8 @@ import sqlite3
 
 def getImages():
 	conn = sqlite3.connect('DobbleImages.sqlite')
+	cursor = conn.cursor()
 	cursor.execute("""SELECT id FROM images""")
 	images = cursor.fetchAll()
+	conn.close()
 	return images
