e4fa4856b9f4928272c81a66e5384e42fc835a59
adubre01 <aubin.dubreuil@etudiant.univ-lr.fr>
2018-03-15 10:38:07 +0100

    Ajout CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import connexion
+from flask_cors import CORS
 from .encoder import JSONEncoder
 
 
@@ -8,4 +9,7 @@ if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Card generation API'})
+	
+	CORS(app.app)
+	
     app.run(port=8080)
